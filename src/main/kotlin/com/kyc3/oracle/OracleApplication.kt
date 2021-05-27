199193381204aba8be9b619a886bf29eb4fce681
igor <igor@kyc3.com>
2021-05-27 20:13:52 +0200

    [PEER-51] Initial commit

---
+++ src/main/kotlin/com/kyc3/oracle/OracleApplication.kt
@@ -0,0 +1,11 @@
+package com.kyc3.oracle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OracleApplication
+
+fun main(args: Array<String>) {
+    runApplication<OracleApplication>(*args)
+}
---
+++ src/test/kotlin/com/kyc3/oracle/OracleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kyc3.oracle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OracleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
