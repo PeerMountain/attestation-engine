e74225c3e12a7d6df5137a4273e9bf24044f41f4
igor <igor@kyc3.com>
2021-06-03 13:18:09 +0200

    [PEER-51] Add JOOQ configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,14 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-jooq")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+  implementation("org.springframework.boot:spring-boot-starter-jooq")
+
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
   implementation("org.flywaydb:flyway-core")
+
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
---
+++ src/main/kotlin/com/kyc3/oracle/config/JooqConfiguration.kt
@@ -0,0 +1,29 @@
+package com.kyc3.oracle.config
+
+import org.jooq.impl.DataSourceConnectionProvider
+import org.jooq.impl.DefaultConfiguration
+import org.jooq.impl.DefaultDSLContext
+import org.jooq.impl.DefaultExecuteListenerProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
+import javax.sql.DataSource
+
+@Configuration
+class JooqConfiguration {
+
+  @Bean
+  fun connectionProvider(dataSource: DataSource): DataSourceConnectionProvider =
+      DataSourceConnectionProvider(TransactionAwareDataSourceProxy(dataSource))
+
+  @Bean
+  fun configuration(
+      connectionProvider: DataSourceConnectionProvider
+  ): DefaultConfiguration =
+      DefaultConfiguration()
+          .also { it.set(connectionProvider) }
+
+  @Bean
+  fun dsl(configuration: DefaultConfiguration): DefaultDSLContext =
+      DefaultDSLContext(configuration)
+}
