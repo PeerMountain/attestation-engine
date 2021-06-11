aedbc15e3619c16ba19bafc93756f28d35b31cf9
Igor Drozdov <igor@kyc3.com>
2021-06-11 19:39:58 +0200

    [PEER-83] Move credentials to application properties

---
+++ src/main/kotlin/com/kyc3/oracle/config/properties/Web3JProperties.kt
@@ -0,0 +1,10 @@
+package com.kyc3.oracle.config.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "web3")
+class Web3JProperties {
+  lateinit var host: String
+}
