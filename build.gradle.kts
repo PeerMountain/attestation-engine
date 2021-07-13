b2bf7a01292d9cd68efcad3cc9745cfde17b7f85
Igor Drozdov <igor@kyc3.com>
2021-07-13 18:25:07 +0200

    [PEER-92] Restructure Proto definitions

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
   implementation("org.web3j:core:5.0.0")
   implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
-  implementation("com.kyc3:oracle-definitions:320ab83")
+  implementation("com.kyc3:oracle-definitions:b8b182b")
 
   runtimeOnly("org.postgresql:postgresql")
 
