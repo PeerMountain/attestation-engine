3847c524645a2233cee80433c6008aa5803e7dc5
Igor Drozdov <igor@kyc3.com>
2021-07-15 19:30:14 +0200

    [PEER-76] Set up xmpp channel between AP and Oracle

--- src/main/kotlin/com/kyc3/oracle/api/OracleAPIResponse.kt
+++ src/main/kotlin/com/kyc3/oracle/api/OracleAPIResponse.kt
@@ -5,18 +5,16 @@ import com.google.protobuf.GeneratedMessageV3
 import com.kyc3.oracle.OracleMessageOuterClass
 import org.jivesoftware.smack.chat2.Chat
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 class OracleAPIResponse {
 
   fun responseToClient(chat: Chat, message: GeneratedMessageV3) =
-      OracleMessageOuterClass.OracleMessage.newBuilder()
-          .setType("type")
-          .setMessage(Any.pack(message))
-          .build()
-          .toByteArray()
-          .let { Arrays.toString(it) }
-          .let { chat.send(it) }
+    OracleMessageOuterClass.OracleMessage.newBuilder()
+      .setType("type")
+      .setMessage(Any.pack(message))
+      .build()
+      .toByteArray().joinToString(",")
+      .let { chat.send(it) }
 
 }
