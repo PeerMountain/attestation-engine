031100831c52533b72926b426cf715d80569c199
Igor Drozdov <igor@kyc3.com>
2021-07-16 14:12:52 +0200

    [PEER-76] Return challenge to sign

---
+++ src/main/kotlin/com/kyc3/oracle/api/router/InitiateNFTPurchaseResponseListener.kt
@@ -0,0 +1,31 @@
+package com.kyc3.oracle.api.router
+
+import com.google.protobuf.Any
+import com.kyc3.ap.challenge.GenerateChallenge
+import com.kyc3.oracle.service.OracleFrontService
+import com.kyc3.oracle.user.InitiateNftPurchase
+import org.jivesoftware.smack.chat2.Chat
+import org.springframework.stereotype.Component
+
+@Component
+class InitiateNFTPurchaseResponseListener(
+  private val oracleFrontService: OracleFrontService
+) :
+  OracleListener<GenerateChallenge.GenerateChallengeResponse, InitiateNftPurchase.InitiateNFTPurchaseResponse> {
+  override fun type(): Class<GenerateChallenge.GenerateChallengeResponse> =
+    GenerateChallenge.GenerateChallengeResponse::class.java
+
+  override fun accept(event: Any, chat: Chat): InitiateNftPurchase.InitiateNFTPurchaseResponse? {
+    event.unpack(type())
+      .let {
+        oracleFrontService.sendToFrontend(
+          InitiateNftPurchase.InitiateNFTPurchaseResponse.newBuilder()
+            .setUserAddress(/*TODO*/ "address")
+            .setNftType(/*TODO*/1)
+            .setChallenge(it.challenge.data)
+            .build()
+        )
+      }
+    return null
+  }
+}
