cd6cdcd289c7d9851d558e84091b8fff3e858ded
Igor Drozdov <igor@kyc3.com>
2021-11-26 11:13:07 +0100

    [PEER] User registration check

--- src/main/kotlin/com/kyc3/oracle/api/router/anonymous/RegisterUserListener.kt
+++ src/main/kotlin/com/kyc3/oracle/api/router/anonymous/RegisterUserListener.kt
@@ -2,6 +2,8 @@ package com.kyc3.oracle.api.router.anonymous
 
 import com.kyc3.Message
 import com.kyc3.oracle.api.router.OracleAnonymousListener
+import com.kyc3.oracle.service.UserLoginChallengeService
+import com.kyc3.oracle.service.Web3Service
 import com.kyc3.oracle.service.XMPPService
 import com.kyc3.oracle.user.Register
 import org.jivesoftware.smack.chat2.Chat
@@ -9,18 +11,24 @@ import org.springframework.stereotype.Component
 
 @Component
 class RegisterUserListener(
-    private val xmppService: XMPPService
+    private val xmppService: XMPPService,
+    private val userLoginChallengeService: UserLoginChallengeService,
+    private val web3Service: Web3Service
 ) : OracleAnonymousListener<Register.RegisterUserRequest, Register.RegisterUserResponse> {
     override fun type(): Class<Register.RegisterUserRequest> =
         Register.RegisterUserRequest::class.java
 
     override fun accept(event: Message.SignedAnonymousMessage, chat: Chat): Register.RegisterUserResponse? =
         event.message.unpack(type()).let { request ->
-            xmppService.createAccount(request.address, request.signedChallenge)
-                .let {
-                    Register.RegisterUserResponse.newBuilder()
-                        .setAddress(request.address)
-                        .build()
+            userLoginChallengeService.userLoginChallengeByAddress(request.address)
+                ?.takeIf { web3Service.verifySignature(it.challenge, request.signedChallenge, request.address) }
+                ?.let {
+                    xmppService.createAccount(request.address, request.signedChallenge)
+                        .let {
+                            Register.RegisterUserResponse.newBuilder()
+                                .setAddress(request.address)
+                                .build()
+                        }
                 }
         }
 }
