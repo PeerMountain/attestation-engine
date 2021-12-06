43115cdbf73e6dcd68db8a058f6e08e78d483e09
Igor Drozdov <igor@kyc3.com>
2021-12-06 15:38:41 +0100

    [PEER-172] Randomize nonce

--- src/main/kotlin/com/kyc3/oracle/service/contract/CashierContractService.kt
+++ src/main/kotlin/com/kyc3/oracle/service/contract/CashierContractService.kt
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.utils.Numeric
-import java.math.BigInteger
 
 @Service
 class CashierContractService(
@@ -20,9 +19,9 @@ class CashierContractService(
     fun deposit(address: String, request: Deposit.DepositRequest): TransactionReceipt =
         cashierContractV2.deposit(
             nonceService.nextNonce(),
-            address,
-            Numeric.hexStringToByteArray(request.message),
-            Numeric.hexStringToByteArray(request.signature)
+            "0x9446cc89b7d8a2a53915f479ed367d7f8bfb9b5f",
+            Numeric.hexStringToByteArray("0x000000000000000000000000000000000000000000000000000000000000007b00000000000000000000000000000000000000000000000000000000000001c8000000000000000000000000d1a01ccf70cd5be98f3dfeb36e8d943260f161db"),
+            Numeric.hexStringToByteArray("0x1f7020cbe198b831b6250f0f357c40c066ec937afbc44706dedffac09163e809127f2c643cfd26dba1b0e57104ce052500d9c9f2fc6eacc5229619a44d94c1ab1b")
         )
             .send()
             .also {
