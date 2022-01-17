ddb5adf9f1963fb3df56938ccc308cab8e7296c0
Igor Drozdov <igor@kyc3.com>
2022-01-17 11:35:35 +0100

    [PEER-152] Update peer chain library version

--- src/main/kotlin/com/kyc3/oracle/api/router/addressed/AttestationProviderDataRequestListener.kt
+++ src/main/kotlin/com/kyc3/oracle/api/router/addressed/AttestationProviderDataRequestListener.kt
@@ -6,7 +6,6 @@ import com.kyc3.oracle.ap.Data
 import com.kyc3.oracle.api.router.OracleAddressedListener
 import com.kyc3.oracle.service.AttestationProviderService
 import org.jivesoftware.smack.chat2.Chat
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
